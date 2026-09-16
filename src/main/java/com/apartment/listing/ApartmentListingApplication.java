b16f20eb8dbf38dfa42ca7d807902705e4158f9b
FathimaAmeera <fathimaameera983@gmail.com>
2026-09-16 16:07:23 +0530

    Initialize apartment sales system

---
+++ src/main/java/com/apartment/listing/ApartmentListingApplication.java
@@ -0,0 +1,12 @@
+package com.apartment.listing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentListingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApartmentListingApplication.class, args);
+    }
+}
